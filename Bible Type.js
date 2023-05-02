d865424cf1a60563c3f65a356133aa88ae1cb817
ToysmartWeb <132273901+ToysmartWeb@users.noreply.github.com>
2023-05-02 04:24:02 -0700

    Add files via upload

---
+++ Bible Type.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+	strings: ["Enjoy Reading the Bible, God's Word", "The Bible deals with every experience in life"],
+	typeSpeed: 100,
+	backSpeed:100,
+	backdelay:1000,
+	loop:true
+})
